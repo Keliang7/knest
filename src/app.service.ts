8d9dba9041c6e30ed7bf200833fb8a0a44a5e237
liangqi <1739484858@qq.com>
2025-08-09 00:57:39 +0800

    feat: 对于service和controller的理解

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
-@Injectable()
+@Injectable() // 表示这个类是一个服务，可以被注入到其他类中
 export class AppService {
   getHello(): string {
     return 'Hello World!';
--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -9,5 +9,6 @@ import { User } from './user.entity';
   imports: [TypeOrmModule.forFeature([User]), AuthModule],
   controllers: [UserController],
   providers: [UserService],
+  // providers: [UserService] 是告诉 NestJS：“我要用这个服务，帮我管理它的生命周期，并支持注入。”
 })
 export class UserModule {}
